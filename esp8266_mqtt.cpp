57ce45f9da6983dbcb333e9d80df2b475af1836c
ajaybnl <ajaybnl@gmail.com>
2019-08-21 18:56:49 +0530

    updates

--- esp8266_mqtt.cpp
+++ esp8266_mqtt.cpp
@@ -4,29 +4,75 @@
 
 <#$#i#$#>
 //MQTT
-#define AIO_SERVER      "192.168.100.17"
+#define AIO_SERVER      "192.168.100.100"
 #define AIO_SERVERPORT  1883    // use 8883 for SSL
 #define AIO_USERNAME    ""
 #define AIO_KEY         ""
-#define PROJECT_NAME "esp8266/temp3"
+
+//Unique ID for each device
+#define CLIENTID "myclient9"
+
+#define FLOW "esp8266"
+
+#define BUTTON1 FLOW "/button1"
+
+#define SENSOR1 FLOW "/sensor1"
+#define SENSOR2 FLOW "/sensor2"
+#define SENSOR3 FLOW "/sensor3"
+
+
+//Do not change
+#define BUTTON1INIT BUTTON1 "/init"
+// My own Retain Script (NODE-RED) Send last value on Reconnect
+//More info : https://pastebin.com/WFW7ArSQ
+
 
 #define mqttdebug true
 <#$#v#$#>
 //MQTT
 WiFiClient client2;
-Adafruit_MQTT_Client mqtt(&client2, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-Adafruit_MQTT_Publish temp1 = Adafruit_MQTT_Publish(&mqtt,PROJECT_NAME );
+Adafruit_MQTT_Client mqtt(&client2, AIO_SERVER, AIO_SERVERPORT,CLIENTID, AIO_USERNAME, AIO_KEY);
+
+//INPUT ONLY BUTTON1
+Adafruit_MQTT_Subscribe button1 = Adafruit_MQTT_Subscribe(&mqtt,BUTTON1);
+
+Adafruit_MQTT_Publish sensor1 = Adafruit_MQTT_Publish(&mqtt,SENSOR1 );
+Adafruit_MQTT_Publish sensor2 = Adafruit_MQTT_Publish(&mqtt,SENSOR2 );
+Adafruit_MQTT_Publish sensor3 = Adafruit_MQTT_Publish(&mqtt,SENSOR3 );
+
+
+//Disable if you don't have init on boot feature
+Adafruit_MQTT_Publish button1init = Adafruit_MQTT_Publish(&mqtt,BUTTON1INIT );
+
+
 
 
 <#$#s#$#>
 
 <#$#sb#$#>
 //MQTT
-mqttconnect();
+
+//Subscribe BEFORE connecting
+mqtt.subscribe(&button1);
+ 
 <#$#l#$#>
 
 <#$#lb#$#>
 
+if(mqttconnect()){
+//Connected	
+}
+
+//MQTT Subscription
+Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    if (subscription == &button1) {
+     int val=button1.lastread;
+	 
+    }
+  }
+  
+  
  //MQTT
  int val=analogRead(A0);
     if (mqttconnect()) {
@@ -51,17 +97,19 @@ bool mqttconnect() {
   if (mqttdebug) Serial.print("Connecting to MQTT... ");
 
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+  while ((ret = mqtt.connect()) != 0) { 
     if (mqttdebug) Serial.println(mqtt.connectErrorString(ret));
     if (mqttdebug) Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
-    delay(2000);  // wait 5 seconds
+    delay(2000); 
     retries--;
     if (retries == 0) {
-      // basically die and wait for WDT to reset me
       return (false);
     }
   }
-
+  
+	button1init.publish(1);  
+	
+  
   if (mqttdebug) Serial.println("MQTT Connected!");
 }
---
+++ esp8266_pubsub_mqtt.cpp
@@ -0,0 +1,111 @@
+//MQTT
+#include <PubSubClient.h>
+
+<#$#i#$#>
+//MQTT
+const char* MQTTSERVER = "192.168.100.100";
+const int   MQTTPORT	   =        1883;
+
+WiFiClient espClient;
+
+PubSubClient mqtt(espClient);
+<#$#v#$#>
+//MQTT
+
+//ClientID will be auto generated on start
+
+//MCU
+#define FLOW "esp8266"
+
+#define BUTTON1 FLOW "/button1"
+
+#define SENSOR1 FLOW "/sensor1"
+#define SENSOR2 FLOW "/sensor2"
+#define SENSOR3 FLOW "/sensor3"
+
+//enable serial debug
+#define mqttdebug true
+
+long mqtttimer = 0;
+
+//will receive button value from subscription
+int button1val=0;
+
+
+<#$#s#$#>
+
+<#$#sb#$#>
+//MQTT
+mqtt.setServer(MQTTSERVER, MQTTPORT);
+mqtt.setCallback(mqttcallback);
+if(mqttdebug) Serial.println("Started");
+randomSeed(micros());
+ 
+<#$#l#$#>
+
+<#$#lb#$#>
+ if (!mqtt.connected()) {
+    mqttconnect();
+  }
+  
+  mqtt.loop();
+
+  
+  if(((millis()-mqtttimer)/1000)>3){
+	
+	char msg[10];    
+    snprintf(msg, 10, "%2d", val);  
+	 mqtt.publish(SENSOR1, msg);    
+	mqtttimer=millis();
+  }
+
+ //buttonval1 is button state
+ 
+
+
+<#$#f#$#>
+//MQTT
+
+
+void mqttcallback(char* topic, byte* payload, unsigned int length) {
+if(mqttdebug){
+Serial.println(topic);
+if(strcmp(topic,BUTTON1)==0){
+ button1val=atoi((char*)payload);
+ Serial.print("Button1: ");
+ Serial.println(button1val);
+ }
+}
+}
+
+void mqttconnect() {
+  int tries=0;
+  while (!mqtt.connected()) {
+    String mqttId = "ESP8266mqtt-";
+    mqttId += String(random(0xffff), HEX);
+        if (mqtt.connect(mqttId.c_str())) {
+			
+			// My own Retain Script (NODE-RED) Send last value on Reconnect
+			//More info : https://pastebin.com/WFW7ArSQ
+
+			mqtt.publish(BUTTON1 "/init","1");
+			
+			
+			mqtt.subscribe(BUTTON1);
+			
+		}else{tries++; if(tries>10)return;}
+    delay(2000);
+    }
+  }
+  
+char* convchar(int value){
+	char msg[10];    
+    snprintf(msg, 10, "%ld", value);	
+	return(msg);
+}
+
+char* convchar(float value){
+	char msg[10];    
+    snprintf(msg, 10, "%ld", value);	
+	return(msg);
+}
